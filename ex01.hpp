42be254fa11e882f5f149deed264afd02530c435
v-like-engine <cypepv@yandex.ru>
2022-03-07 17:18:23 +0300

    Fix sum in ex01 again

--- ex01.hpp
+++ ex01.hpp
@@ -11,7 +11,7 @@ template < typename T >
 T multiples (T& sum, T x, T n)
 {
     sum = 1;
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i <= n; i++){
         sum += x * i;
     }
     return sum;
