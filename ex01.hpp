76d4a4bd226feaf8d2b62dd9060d8e6b2e0b5ec0
v-like-engine <cypepv@yandex.ru>
2022-03-07 17:07:44 +0300

    Remove public

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,13 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-void multiples (int& sum, int x, int n)
-{
+//void multiples (int& sum, int x, int n)
+//{
+//
+//}
 
+template < typename T >
+T multiples (T& sum, T x, T n)
+{
+    sum = x * n;
 }
