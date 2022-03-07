64104ca2bb75691211da74bebe809bf8519e21a1
v-like-engine <cypepv@yandex.ru>
2022-03-07 17:11:02 +0300

    Remove public again

--- ex02.hpp
+++ ex02.hpp
@@ -2,7 +2,7 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template < typename T >
-public class Calculator {
+class Calculator {
 private:
     T num1, num2;
 
